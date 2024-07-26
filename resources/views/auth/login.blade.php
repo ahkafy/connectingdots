e5699d16672bcaa049fa1f6e66c489d03c3dd65d
Abdullah hel kafy <ahkafy@gmail.com>
2024-07-26 19:36:05 +0600

    Dashboard Design

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -4,15 +4,30 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <title>MEMBER'S LOGIN - CONNECTING DOTS BANGLADESH</title>
+  <title>DORYBOOK LOGIN</title>
 
+  <meta content="We are Cloud House&trade; Technologies, actively participating in the market with IT Services and Business Solutions since 2017." name="description">
+  <meta content="Cloud House, Best IT Company, Web Development, Web Design, Cheap eCommerce Website Design, Best Website Design, Digital marketing, Internet marketing, Digital marketing agency, Digital marketing services, Digital marketing Company, Social media marketing, SEO digital marketing, Online marketing, Social media marketing in Bangladesh, Full stack digital marketing agency" name="keywords">
+  <meta name="theme-color" content="#18505f">
+ <meta  name="simpledcver"  content="eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJkb21haW4iOiJjbG91ZGhvdXNlYmQuY29tIiwiZXhwIjoxNjY4OTg4ODAwfQ.nkk7TGrdpAvaOHVdWNIthhmjTNiKayTW3xaYptYG0QU">
+
+
+  <meta property="og:url"                content="https://cloudhousebd.com" />
+  <meta property="og:type"               content="website" />
+  <meta property="og:title"              content="Cloud House&trade; Technologies Bangladesh - Complete IT Service and Business Solution" />
+  <meta property="og:description"        content="We are actively participating in the market with IT Services and Business Solutions since 2017."/>
+  <meta property="og:image"              content="https://cloudhousebd.com/logo.png" />
+  <meta property="og:image:alt"          content="Cloud House&trade;" />
+  <meta property="fb:app_id"             content="223274071563650" />
+
+  <link rel="canonical" href="https://cloudhousebd.com" />
 
   <!-- Favicons -->
-  <link href="assets/img/favicon.png" rel="icon">
+  <link href="{{ asset('assets/img/favicon.png') }}" rel="icon">
 
 
   <link href="https://fonts.googleapis.com/css?family=Karla:400,700&display=swap" rel="stylesheet">
-  <link rel="stylesheet" href="https://cdn.materialdesignicons.com/4.8.95/css/materialdesignicons.min.css">
+  <link rel="stylesheet" href="{{ asset('assets/vendors/mdi/css/materialdesignicons.min.css') }}">
   <link rel="stylesheet" href="{{ url('build/assets/app-D-sv12UV.css') }}">
   <link rel="stylesheet" href="{{ url('assets/css/login.css') }}">
 </head>
@@ -22,10 +37,10 @@
       <div class="row">
         <div class="col-sm-6 login-section-wrapper">
           <div class="brand-wrapper">
-            <img src="assets/images/logo.svg" alt="logo" class="logo">
+           <a href="{{ url('/') }}"><img src="assets/images/logo.svg" alt="logo" class="logo"></a>
           </div>
           <div class="login-wrapper my-auto">
-            <h1 class="login-title">Log in</h1>
+            <h3 class="login-title">LOG IN</h3>
             <form action="{{ route('login') }}" method="POST">
                 @csrf
               <div class="form-group">
@@ -54,7 +69,7 @@
                 <a class="forgot-password-link" href="{{ route('password.request') }}">{{ __('Forgot Your Password?') }}</a>
             @endif
 
-            <p class="login-wrapper-footer-text">Don't have an account? <a href="#!" class="text-reset">Register here</a></p>
+            <p class="login-wrapper-footer-text">Don't have an account? <a href="{{ route('register') }}" class="text-reset">Register here</a></p>
           </div>
         </div>
         <div class="col-sm-6 px-0 d-none d-sm-block">
---
+++ resources/views/layouts/template.blade.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title>{{ $title ?? 'Dashboard' }} - Connecting Dots Member's Home</title>
+
+    <link rel="stylesheet" href="{{ asset('assets/vendors/feather/feather.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/vendors/mdi/css/materialdesignicons.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/vendors/ti-icons/css/themify-icons.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/vendors/font-awesome/css/font-awesome.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/vendors/typicons/typicons.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/vendors/simple-line-icons/css/simple-line-icons.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/vendors/css/vendor.bundle.base.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/vendors/bootstrap-datepicker/bootstrap-datepicker.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/css/style.css') }}">
+    <link rel="shortcut icon" href="{{ asset('assets/images/favicon.png') }}">
+
+  </head>
+
+  <body>
+    <div class="container-scroller">
+
+            @include('layouts.nav')
+
+        <div class="container-fluid page-body-wrapper">
+
+            @include('layouts.sidebar')
+
+            <div class="main-panel">
+
+                <div class="content-wrapper">
+                    @yield('content')
+                </div>
+
+                <footer class="footer">
+                    <div class="d-sm-flex justify-content-center justify-content-sm-between">
+                    <span class="text-muted text-center text-sm-left d-block d-sm-inline-block">Developed by <a href="https://www.cloudhousebd.com/" target="_blank">Cloud House Technologies</a> from Micro Initiatives.</span>
+                    <span class="float-none float-sm-end d-block mt-1 mt-sm-0 text-center">Copyright © 2024. All rights reserved by <a href="https://connectingdots.com.bd">Connecting Dots.</a> </span>
+                    </div>
+                </footer>
+
+            </div>
+      </div>
+    </div>
+
+    <script src="{{ asset('assets/vendors/js/vendor.bundle.base.js') }}"></script>
+    <script src="{{ asset('assets/vendors/bootstrap-datepicker/bootstrap-datepicker.min.js') }}"></script>
+    <script src="{{ asset('assets/js/off-canvas.js') }}"></script>
+    <script src="{{ asset('assets/js/template.js') }}"></script>
+    <script src="{{ asset('assets/js/settings.js') }}"></script>
+    <script src="{{ asset('assets/js/hoverable-collapse.js') }}"></script>
+    <script src="{{ asset('assets/js/todolist.js') }}"></script>
+
+  </body>
+</html>
